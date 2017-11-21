14957b920c1987144c5ed9ff80fd15cd75fbbb23
Devin Roth <devin@macbook.local>
2017-11-21 11:31:58 -0800

    Initial commit

---
+++ Framework/macOS/SwiftMIDI/SwiftMIDI.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMIDI.h
+//  SwiftMIDI
+//
+//  Created by Devin Roth on 2017-11-21.
+//  Copyright © 2017 Devin Roth. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftMIDI.
+FOUNDATION_EXPORT double SwiftMIDIVersionNumber;
+
+//! Project version string for SwiftMIDI.
+FOUNDATION_EXPORT const unsigned char SwiftMIDIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMIDI/PublicHeader.h>
+
+
